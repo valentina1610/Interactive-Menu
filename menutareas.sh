ba6d270536fdb8eba94252341b84630ef45ccf8a
valentina <valentinaolmosoo@gmail.com>
2024-11-25 13:05:05 -0300

    menutareas.sh

--- menutareas.sh
+++ menutareas.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# Definir colores
+magenta='\033[1;35m'
+cyan='\033[1;36m'
+rojo='\033[1;31m'
+reset='\033[0m'
+
 while true; do
-  echo -e "\n=== Menú de Tareas ==="
-  echo "1. Generar informe del uso del sistema"
-  echo "2. Crear un usuario nuevo en el sistema y gestionar sus permisos y directorios
-personales."
-  echo "3. Eliminar archivos temporales y caché del sistema y navegadores"
-  echo "4. Salir"
+  echo -e "\n${magenta}=== Menú de Tareas ===${reset}"
+  echo -e "${cyan}1.${reset} Generar informe del uso del sistema"
+  echo -e "${cyan}2.${reset} Crear un usuario nuevo en el sistema y gestionar sus permisos y directorios personales."
+  echo -e "${cyan}3.${reset} Eliminar archivos temporales y caché del sistema y navegadores"
+  echo -e "${cyan}4.${reset} Salir"
   read -r -p "Seleccione una opción: " opcion
   
   case $opcion in
     1) ./informe.sh ;;      
     2) ./usuarios.sh ;;     
     3) ./limpieza.sh ;;     
-    4) echo "Saliendo..."; break ;; 
-    *) echo "Opción no válida. Intente nuevamente." ;;  
+    4) echo -e "${rojo}Saliendo...${reset}"; break ;; 
+    *) echo -e "${rojo}Opción no válida. Intente nuevamente.${reset}" ;;  
   esac
 done
